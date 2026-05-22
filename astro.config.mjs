c73c2fdee208a05b44e67d110cbf87819c1e94df
nam <257072372+kintounam3@users.noreply.github.com>
2026-05-22 22:53:31 +0900

    Add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+
+export default defineConfig({
+  // site: "http://localhost:4321/"
+  site: "https://astrosymphony-minimal.netlify.app/"
+});
