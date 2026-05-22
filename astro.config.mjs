bc739012a31638ceef68b0e3856a046d664dbd77
nam <257072372+kintounam3@users.noreply.github.com>
2026-05-23 00:01:52 +0900

    Include a Preact greeting banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 // export default defineConfig({});
 
 export default defineConfig({
   // site: "http://localhost:4321/"
-  site: "https://astrosymphony-minimal.netlify.app/"
+  site: "https://astrosymphony-minimal.netlify.app/",
+
+  integrations: [preact()]
 });
